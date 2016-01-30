1a00feb0eb9ad38983393a8acc5a11379e21f7e5
Nathan Friend <hello@nathanfriend.io>
2016-01-29 22:56:03 -0600

    working on adding autoprefixing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,37 @@
 module.exports = function (grunt) {
-	'use strict';
+    'use strict';
 
-	grunt.initConfig({
-		watch: {
-			files: 'styles/**/*.less',
-			tasks: ['less:development']
-		},
-		less: {
-			development: {
-				files: {
-					'styles/deck-of-cards.css': 'styles/deck-of-cards.less'
-				}
-			}
-		}
-	});
+    grunt.initConfig({
+        watch: {
+            files: 'styles/**/*.less',
+            tasks: ['less:development', 'postcss:development']
+        },
+        less: {
+            development: {
+                files: {
+                    'styles/deck-of-cards.css': 'styles/deck-of-cards.less'
+                }
+            }
+        },
+        postcss: {
+            options: {
+                map: true,
+                processors: [require('autoprefixer')({ browser: ['ie 11', 'last 2 Chrome versions', 'last 2 Firefox versions'] })]
+            },
+            development: {
+                src: 'styles/deck-of-cards.css'
+            }
+        }
+    });
 
-	grunt.registerTask(
-		'build',
-		'Compiles Less',
-		['watch']
-	)
-
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask(
+        'build',
+        'Compiles Less',
+        ['watch']
+        );
+        
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('autoprefixer');
 }
